cc1c5e3bef22187fcca0467af87cdace7e2c1532
susi <susi.rinortner@gmail.com>
2018-03-12 19:56:58 +0100

    refs #2, improved image validation

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/vehiclerental/service/SimpleVehicleService.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/vehiclerental/service/SimpleVehicleService.java
@@ -15,14 +15,21 @@ import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
+
+import static at.ac.tuwien.sepm.assignment.individual.vehiclerental.util.Validator.validateVehicle;
+import static java.util.List.of;
 
 public class SimpleVehicleService implements VehicleService {
 
+    private static final int FIVE_MEGABYTES = 5 * 1024 * 1024;
+    private static final int MIN_WIDTH = 500;
+    private static final int MIN_HEIGHT = 500;
     private VehicleDAO vehicleDAO;
-    private Validator validator = new Validator();
     private static final Path imageDestinationPath = Paths.get(System.getProperty("user.home"),"/.sepm/images");
     private String currentPictureTitle = null;
+    private static final List<String> SUPPORTED_FILE_TYPES = of(".jpg", ".png");
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().getClass());
 
@@ -40,7 +47,7 @@ public class SimpleVehicleService implements VehicleService {
             currentPictureTitle = addPicture(file);
             vehicle.setPicture(currentPictureTitle);
         }
-        validator.validateVehicle(vehicle);
+        validateVehicle(vehicle);
         return vehicleDAO.addVehicleToDatabase(vehicle);
     }
 
@@ -55,8 +62,11 @@ public class SimpleVehicleService implements VehicleService {
                 BufferedImage picture = ImageIO.read(file);
                 int height = picture.getHeight();
                 int width = picture.getWidth();
-                if (file.length() < 5242880) {
-                    if (height >= 500 && width >= 500) {
+                if(SUPPORTED_FILE_TYPES.stream().noneMatch(ft -> file.getName().toLowerCase().endsWith(ft))) {
+                    throw new IllegalPictureException("Filetype not supported");
+                }
+                if (file.length() < FIVE_MEGABYTES) {
+                    if (height >= MIN_HEIGHT && width >= MIN_WIDTH) {
                         Files.copy(file.toPath(), destinationFile.toPath());
                     } else {
                         LOG.error("The Image doesn't have the required mesurements");
